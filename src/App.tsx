abe3af1e059eeac07661c6dc7ccaed7d96e369fa
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2025-07-10 15:57:25 +0530

    Project Init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello Vite!</h1>
+            <button className="btn btn-primary">Button</button>
+        </div>
+    );
+}
+
+export default App;
