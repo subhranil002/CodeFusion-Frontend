7f56482ea479ff2d27771f24166ad71c4719d67c
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2025-07-17 22:33:08 +0530

    Add handleTyping, codeUpdate

--- src/components/CodePlayground/CodeEditor.tsx
+++ src/components/CodePlayground/CodeEditor.tsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import Editor from "@monaco-editor/react";
 import debounce from "debounce";
+import { useDispatch, useSelector } from "react-redux";
+import editorSocket from "../../configs/EditorSocketConfig";
+import { setCode } from "../../redux/slices/EditorSlice";
 
 type CodeEditorProps = {
     language: string;
-    value?: string;
-    handleChange: (e: string | undefined) => void;
     options?: any;
 };
 
-function CodeEditor({
-    language,
-    value,
-    handleChange,
-    options,
-}: CodeEditorProps) {
+function CodeEditor({ language, options }: CodeEditorProps) {
     const [themeJson, setThemeJson] = useState(null);
+    const { code } = useSelector((state: any) => state.editor);
+    const dispatch = useDispatch();
+    const isTyping = useRef(false);
+    const timeout = useRef<number | null>(null);
 
     useEffect(() => {
         (async () => {
@@ -34,15 +34,53 @@ function CodeEditor({
         monaco.editor.setTheme("darkchai");
     }
 
+    useEffect(() => {
+        editorSocket.on("codeUpdate", (newCode: string) => {
+            dispatch(setCode(newCode));
+        });
+
+        return () => {
+            editorSocket.off("codeUpdate");
+            if (timeout.current) clearTimeout(timeout.current);
+        };
+    }, []);
+
+    function handleTyping() {
+        if (!isTyping.current) {
+            editorSocket.emit("startTyping");
+            isTyping.current = true;
+        }
+
+        if (timeout.current) {
+            clearTimeout(timeout.current);
+        }
+        timeout.current = setTimeout(() => {
+            editorSocket.emit("stopTyping");
+            isTyping.current = false;
+        }, 500);
+    }
+
+    const debouncedChange = useMemo(
+        () =>
+            debounce((value: string | undefined) => {
+                editorSocket.emit("codeChange", { code: value });
+            }, 500),
+        []
+    );
+
     return (
         themeJson && (
             <Editor
                 height="100%"
                 width="100%"
                 language={language}
-                defaultValue={value}
+                value={code}
                 onMount={handleEditorTheme}
-                onChange={debounce(handleChange, 500)}
+                onChange={(value) => {
+                    handleTyping();
+                    dispatch(setCode(value ?? ""));
+                    debouncedChange(value);
+                }}
                 options={{
                     wordWrap: "on",
                     fontFamily: "monospace",
