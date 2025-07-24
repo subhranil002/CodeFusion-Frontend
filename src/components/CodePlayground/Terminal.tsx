46c3d266e9f7d538a39d4b6bb5efb0fee12cd5a9
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2025-07-25 02:04:16 +0530

    Add Terminal

---
+++ src/components/CodePlayground/Terminal.tsx
@@ -0,0 +1,72 @@
+import { useEffect } from "react";
+import editorSocket from "../../configs/EditorSocketConfig";
+import { setTerminalData } from "../../redux/slices/EditorSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+function Terminal() {
+    const dispatch = useDispatch();
+    const { terminalData } = useSelector((state: any) => state.editor);
+
+    useEffect(() => {
+        editorSocket.on("updateTerminal", (data: any) => {
+            dispatch(setTerminalData(data));
+        });
+
+        return () => {
+            editorSocket.off("updateTerminal");
+        };
+    }, []);
+
+    return (
+        <div className="mockup-code rounded-t-none w-full h-34 md:h-44 overflow-y-auto p-4">
+            {terminalData ? (
+                <>
+                    {terminalData?.status && (
+                        <pre
+                            data-prefix="$"
+                            className={
+                                terminalData?.status?.id === 3
+                                    ? "bg-success text-success-content font-semibold"
+                                    : "bg-error text-error-content font-semibold"
+                            }
+                        >
+                            <code>{terminalData?.status?.description}</code>
+                        </pre>
+                    )}
+                    {terminalData?.stdout && (
+                        <pre data-prefix=">" className="text-success">
+                            <code>
+                                <span className="font-bold">Output: </span>
+                                {terminalData.stdout}
+                            </code>
+                        </pre>
+                    )}
+
+                    {terminalData?.stderr && (
+                        <pre data-prefix="!" className="text-error">
+                            <code>
+                                <span className="font-bold">Error: </span>
+                                {terminalData.stderr}
+                            </code>
+                        </pre>
+                    )}
+
+                    {terminalData?.status?.id === 3 && terminalData?.time && (
+                        <pre data-prefix="~" className="text-info">
+                            <code>
+                                <span className="font-bold">Time: </span>
+                                {terminalData.time} ms
+                            </code>
+                        </pre>
+                    )}
+                </>
+            ) : (
+                <pre data-prefix="$" className="text-warning">
+                    <code>Run code to see output...</code>
+                </pre>
+            )}
+        </div>
+    );
+}
+
+export default Terminal;
--- src/redux/slices/EditorSlice.ts
+++ src/redux/slices/EditorSlice.ts
@@ -1,21 +1,39 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
-
-type User = {
-    name: string;
-    isTyping: boolean;
-};
-
-type InitialState = {
-    users: User[]; // ← array of User
-    code: string;
-    language: string;
-};
+import {
+    createAsyncThunk,
+    createSlice,
+    type PayloadAction,
+} from "@reduxjs/toolkit";
+import type {
+    User,
+    TerminalData,
+    InitialState,
+    Language,
+} from "../../types/types";
+import getLanguages from "../../apis/CodePlayground/getLanguages";
+import codeRunner from "../../apis/CodePlayground/codeRunner";
+// import editorSocket from "../../configs/EditorSocketConfig";
 
 const initialState: InitialState = {
     users: [],
     code: "// Write code here...",
-    language: "javascript",
+    currentLanguage: null,
+    languageList: [],
+    terminalData: null,
 };
+
+export const fetchLanguages = createAsyncThunk(
+    "editor/getLanguages",
+    async () => {
+        return await getLanguages();
+    }
+);
+
+export const executeCode = createAsyncThunk(
+    "editor/executeCode",
+    async (data: { code: string; langId: number; stdIn?: string }) => {
+        return await codeRunner(data.code, data.langId, data?.stdIn);
+    }
+);
 
 const editorSlice = createSlice({
     name: "editor",
@@ -27,11 +45,26 @@ const editorSlice = createSlice({
         setCode(state, action: PayloadAction<string>) {
             state.code = action.payload;
         },
-        setLanguage(state, action: PayloadAction<string>) {
-            state.language = action.payload;
+        setLanguage(state, action: PayloadAction<Language>) {
+            state.currentLanguage = action.payload;
         },
+        setTerminalData(state, action: PayloadAction<TerminalData>) {
+            state.terminalData = action.payload;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchLanguages.fulfilled, (state, action) => {
+                state.languageList = action.payload.data;
+                state.currentLanguage = action.payload.data[0];
+            })
+            .addCase(executeCode.fulfilled, (state, action) => {
+                state.terminalData = action.payload.data;
+                // editorSocket.emit("updateTerminal", action.payload.data);
+            });
     },
 });
 
-export const { setUsers, setCode, setLanguage } = editorSlice.actions;
+export const { setUsers, setCode, setLanguage, setTerminalData } =
+    editorSlice.actions;
 export default editorSlice;
