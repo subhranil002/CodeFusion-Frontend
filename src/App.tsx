c735e2170a4b42ed52a175c31510a0c8507e441a
subhranil002 <106914208+subhranil002@users.noreply.github.com>
2025-07-27 02:29:06 +0530

    Add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import Router from "./router/Router";
+import { useLocation } from "react-router-dom";
 
 function App() {
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location.pathname]);
+
     return <Router />;
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/Store.ts";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")!).render(
     <Provider store={store}>
         <BrowserRouter>
             <App />
             <Toaster />
+            <Analytics/>
         </BrowserRouter>
     </Provider>
 );
